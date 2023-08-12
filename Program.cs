924f62eb633448c36f42023e0f7d7c582df34dfb
Khalid Sookia <khalid.sookia@iknsa.com>
2023-08-12 22:19:31 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DocGeneratorConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Microsoft.Office.Interop.Word.Application word = new();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
